2e53e868a77436ba9dc28e9886762b9b1a070e88
Charlotte Dujardin <charlotte@dujardin.popschool>
2017-03-09 11:14:19 +0100

    initialisation du projet

---
+++ my_project/README.md
@@ -0,0 +1,4 @@
+my_project
+==========
+
+A Symfony project created on March 8, 2017, 10:18 am.
---
+++ my_project/src/AppBundle/Form/ArticleType.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace AppBundle\Form;
+
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ArticleType extends AbstractType
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+                    ->add('titre')
+                    ->add('corps')
+                    ->add('publication')
+                    ->add('auteur', EntityType::class, [
+                              'class' => 'AppBundle:Auteur',
+                              'choice_label' => 'nom',
+                              ])
+          ;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'AppBundle\Entity\Article'
+        ));
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getBlockPrefix()
+    {
+        return 'appbundle_article';
+    }
+
+
+}
